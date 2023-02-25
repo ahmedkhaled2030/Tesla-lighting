b907274b1a3b749172afeeae271bebf4693e5d5f
codeBindHex <xhunter19192020@gmail.com>
2023-02-25 21:32:54 +0200

    ..

--- components/CategoryCard.jsx
+++ components/CategoryCard.jsx
@@ -2,22 +2,21 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import styles from "./../styles/CategoryCard.module.scss";
-const CategoryCard = ({ img, title ,key}) => {
+const CategoryCard = ({ img, title, key }) => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <Link href={`/collections/1`} passHref>
-          <div >
-          <Image
-            src={img}
-            alt=""
-            width="275"
-            height="275"
-            objectFit="contain"
-          />
-          <h1 className={`secondaryText ${styles.title}`}>{title}</h1>
+          <div>
+            <Image
+              src={img}
+              alt=""
+              width="275"
+              height="275"
+              objectFit="contain"
+            />
+            <h1 className={`secondaryText ${styles.title}`}>{title}</h1>
           </div>
-
         </Link>
       </div>
     </div>
