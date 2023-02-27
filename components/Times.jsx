15477b7890092325d25649add475237eb817f39c
codeBindHex <xhunter19192020@gmail.com>
2023-02-27 22:44:04 +0200

    dashboard add Category + addProduct

--- components/Times.jsx
+++ components/Times.jsx
@@ -1,3 +1,5 @@
+
+
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./../styles/Times.module.scss";
@@ -8,7 +10,8 @@ const Times = ({ timeProps }) => {
       <div className={styles.times}>
         <div dangerouslySetInnerHTML={{ __html: timeProps.text }}></div>
         {/* <Link href={`${timeProps.url}`} >  */}
-        <Link href={`${timeProps.url}`} rel="noopener noreferrer" target="_blank"> 
+        {/* <Link href={`${timeProps.url}`} rel="noopener noreferrer" target="_blank">  */}
+        <Link href="https://goo.gl/maps/89LnzY4QbNtHzuQS7" passHref={true}>
           <button>GET DIRECTIONS</button>
         </Link>
       </div>
