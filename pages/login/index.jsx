29e9a968fec0989d75ecb115261a6aa6205273d7
codeBindHex <xhunter19192020@gmail.com>
2023-03-14 20:09:57 +0200

    loginFix

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -58,10 +58,10 @@ const Login = () => {
       errorMessage:
         "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
       label: "PASSWORD",
-      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      // pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
       required: true,
     },
-  ];
+  ]; 
 
   const handleSubmit = (e) => {
     e.preventDefault();
